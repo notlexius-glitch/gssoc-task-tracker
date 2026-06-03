e210fe3552bbac5cde8a9ba6111e430c0ecd59ea
Atul <notlexius@gmail.com>
2026-06-03 13:01:37 +0530

    Add Task Tracker projects

---
+++ script.js
@@ -0,0 +1,16 @@
+function addTask() {
+  const input = document.getElementById("taskInput");
+  const text = input.value.trim();
+
+  if (text === "") return;
+
+  const li = document.createElement("li");
+  li.innerHTML = `
+    ${text}
+    <button onclick="this.parentElement.remove()">❌</button>
+  `;
+
+  document.getElementById("taskList").appendChild(li);
+
+  input.value = "";
+}
